053daa5bade76a57474853aa4370453586bb5474
Victor <53414263+vuduland@users.noreply.github.com>
2020-05-05 20:47:05 -0500

    minutes timer working inside app.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import './App.scss';
 function App() {
   const [timer, setTimer] = useState({
     name: 'initial timer',
-    time: 0,
+    time: 120,
+    minutes() {
+      return Math.floor(this.time / 60);
+    },
+    seconds() {
+      return this.time % 60; //<= 60 ? this.time : 60
+    },
     active: true,
   });
 
   useEffect(() => {
     const handleTimerChange = setTimeout(() => {
-      setTimer({ ...timer, time: timer.time + 1 });
+      setTimer({ ...timer, time: timer.time - 1 });
     }, 1000);
   }, [timer]);
 
   // Only re-run the effect if timerType changes
-  return <div>{timer.time}</div>;
+  return (
+    <div>
+      <div className='block'>
+        <span>Minutes:</span>
+        <span>
+          {timer.minutes()}:{timer.seconds().toFixed(0)}
+        </span>
+      </div>
+      {timer.time}
+    </div>
+  );
 }
 
 export default App;
