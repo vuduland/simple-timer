25ede0c5449e750ba323053c88a3fc0edc74a5f4
Victor <53414263+vuduland@users.noreply.github.com>
2020-05-05 22:33:20 -0500

    in the fugue of getting start to go to current time

--- src/App.js
+++ src/App.js
@@ -4,34 +4,64 @@ import './App.scss';
 function App() {
   const [timer, setTimer] = useState({
     name: 'initial timer',
-    time: 120,
+    time: 600000, // 6
+    start: 0,
+    current: 0,
+    end: 0,
+    total: 0,
+
     minutes() {
-      return Math.floor(this.time / 60);
+      return Math.floor((this.elapsed() / 60000) % 60);
     },
     seconds() {
-      return this.time % 60; //<= 60 ? this.time : 60
+      return Math.floor((this.elapsed() / 1000) % 60);
     },
-    active: true,
+    elapsed() {
+      return (this.time - (this.current - this.start);
+    },
   });
+  const [timerActive, setTimerActive] = useState('stopped');
+
+  let handleTimerChange = null;
 
   useEffect(() => {
-    const handleTimerChange = setTimeout(() => {
-      setTimer({ ...timer, time: timer.time - 1 });
-    }, 1000);
-  }, [timer]);
+    if (timerActive === 'starting') {
+      setTimer({ ...timer, total: timer.total + timer.elapsed(), start: Date.now() });
+      setTimerActive('active');
+    }
+    handleTimerChange = setTimeout(() => {
+      if (timerActive === 'active') {
+        setTimer({ ...timer, current: Date.now() }); // Date.now() === miliseconds?
+      }
+    }, 1);
+    if (timerActive === 'stopped') {
+      clearTimeout(handleTimerChange);
+    }
+  }, [timer, timerActive]);
 
   // Only re-run the effect if timerType changes
   return (
     <div>
-      <div className='block'>
-        <span>Minutes:</span>
-        <span>
-          {timer.minutes()}:{timer.seconds().toFixed(0)}
-        </span>
+      <div className='container'>
+        <div className='timerblock'>
+          <span>Time:</span>
+          <span>
+            {timer.minutes()}:{timer.seconds().toFixed(0)}
+            <br></br>
+            {timer.elapsed().toFixed(0)}
+          </span>
+          <div className='bottom'>
+            <button
+              onClick={() => setTimerActive(timerActive === 'active' ? 'stopped' : 'starting')}>
+              {timerActive === 'active' ? `stop` : `start`}
+            </button>
+          </div>
+        </div>
       </div>
-      {timer.time}
     </div>
   );
 }
+// :{timer.miliseconds().toFixed(0)}
+console.log(`Date.now() === ${Date.now()}`);
 
 export default App;
