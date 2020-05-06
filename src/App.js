c732f5663ebc54db894c5de19e491f7d9d3fb8a6
Victor <53414263+vuduland@users.noreply.github.com>
2020-05-05 20:31:08 -0500

    timer fucking works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
-  return <div>test</div>;
+  const [timer, setTimer] = useState({
+    name: 'initial timer',
+    time: 0,
+    active: true,
+  });
+
+  useEffect(() => {
+    const handleTimerChange = setTimeout(() => {
+      setTimer({ ...timer, time: timer.time + 1 });
+    }, 1000);
+  }, [timer]);
+
+  // Only re-run the effect if timerType changes
+  return <div>{timer.time}</div>;
 }
 
 export default App;
